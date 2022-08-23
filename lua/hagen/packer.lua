cefa3b341db2b7e730a286bc8a3fe313863df2e6
Robert Hagen <hagen.robert@fronius.com>
2022-08-23 15:34:48 +0200

    packer comments

--- lua/hagen/packer.lua
+++ lua/hagen/packer.lua
@@ -2,36 +2,40 @@ return require("packer").startup(function()
 
     use("wbthomason/packer.nvim")
 
+    -- color schemes
     use("gruvbox-community/gruvbox")
     use("folke/tokyonight.nvim")
     use("dracula/vim", { as = "dracula" })
 
+    -- useful
     use("christoomey/vim-tmux-navigator")
     use("mbbill/undotree")
+
     use("tpope/vim-commentary")
     use("tpope/vim-surround")
 
+    -- treesitter
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
     use("nvim-treesitter/nvim-treesitter-angular")
     use("glench/vim-jinja2-syntax")
 
+    -- telescope
     use("nvim-lua/plenary.nvim")
     use("nvim-lua/popup.nvim")
     use("nvim-telescope/telescope.nvim")
     use("kyazdani42/nvim-web-devicons")
 
+    -- lsp goodness
     use("neovim/nvim-lspconfig")
-    use("tzachar/cmp-tabnine", { run = "./install.sh", requires = 'hrsh7th/nvim-cmp' })
     use("onsails/lspkind-nvim")
     use("glepnir/lspsaga.nvim")
     use("simrat39/symbols-outline.nvim")
-
-    use("L3MON4D3/LuaSnip")
-    use("rafamadriz/friendly-snippets")
-
     use("hrsh7th/nvim-cmp")
     use("hrsh7th/cmp-nvim-lsp")
     use("hrsh7th/cmp-buffer")
+    use("tzachar/cmp-tabnine", { run = "./install.sh", requires = 'hrsh7th/nvim-cmp' })
     use("saadparwaiz1/cmp_luasnip")
+    use("L3MON4D3/LuaSnip")
+    use("rafamadriz/friendly-snippets")
 
 end)
