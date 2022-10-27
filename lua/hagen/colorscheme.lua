f243516f7038a61d00ce935e1535c7d5941f4bf9
Robert Hagen <hagen.robert@protonmail.com>
2022-10-27 16:50:54 +0200

    tokynight night color

--- lua/hagen/colorscheme.lua
+++ lua/hagen/colorscheme.lua
@@ -1,7 +1,4 @@
-local colorscheme = "tokyonight"
-
-vim.g.tokyonight_style = "storm"
-vim.opt.background = "dark"
+local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
